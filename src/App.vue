c8babc5576944834e0d8531f4e002b573d4668a2
ZHENGGEGE <593012736@qq.com>
2017-10-11 13:58:25 +0800

    manage

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  
+
+}
+</script>
+
+<style>
+  @import 'assets/css/main.css';
+ 
+</style>
+
+
+
+
+
+
+
---
+++ src/router/index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Login from 'components/page/login/login'
+import Home from 'components/common/home/home'
+import Readme from 'components/page/readme/readme'
+import Basetable from 'components/page/basetable/basetable'
+import Baseform from 'components/page/baseform/baseform'
+
+
+Vue.use(Router)
+
+const routes = [
+    {
+		path: '/',
+		component: Login
+    },
+    {
+        path: '/readme',
+        component: Home ,
+        children:[
+            {
+                path: '/readme',
+                component: Readme
+            },
+            {
+                path: '/basetable',
+                component: Basetable
+            },
+            {
+                path: '/baseform',
+                component: Baseform
+            }
+
+        ]
+    },
+    {
+        path: '/login',    
+        component: Login
+    }
+
+]
+
+
+export default new Router({
+    routes
+})
