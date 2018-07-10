58afea649b421b63b8e9d77893b83fd7cd9b875c
ZHENGGEGE <593012736@qq.com>
2018-07-10 11:24:13 +0800

    feat:删除不用代码

--- src/store/modules/application.js
+++ src/store/modules/application.js
@@ -1,15 +1,13 @@
 import * as types from '../mutation-types'
 const application = {
   state: {
-    applicationList: []
+
   },
   getters: {
-    applicationList: state => state.applicationList
+
   },
   mutations: {
-    [types.SET_APPLICATION_LIST] (state, list) {
-      state.applicationList = list
-    }
+
   },
   actions: {
 
